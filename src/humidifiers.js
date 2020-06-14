7cc23df06d730e16c54cb9cfe94cbc1073934fef
Dedicated <dedi@dedi.ch>
2020-06-14 07:31:33 +0200

    fix import filename

--- src/humidifiers.js
+++ src/humidifiers.js
@@ -1,5 +1,5 @@
 import ZHIMI_HUMIDIFIER_CB1 from './configurations/zhimi_humidifier_cb1';
-import DEERMA_HUMIDIFIER_MJJSQ from './configurations/deerma.humidifier.mjjsq';
+import DEERMA_HUMIDIFIER_MJJSQ from './configurations/deerma_humidifier_mjjsq';
 
 const HUMIDIFIERS = {
   default: ZHIMI_HUMIDIFIER_CB1,
